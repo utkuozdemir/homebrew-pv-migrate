1d778c693a1851fccab6b4a2877b6c8ef4adced4
goreleaserbot <goreleaser@carlosbecker.com>
2021-05-23 02:17:53 +0200

    Brew formula update for pv-migrate version v0.5.4

--- Formula/pv-migrate.rb
+++ Formula/pv-migrate.rb
@@ -10,16 +10,16 @@ class PvMigrate < Formula
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/utkuozdemir/pv-migrate/releases/download/v0.5.4/pv-migrate_v0.5.4_darwin_x86_64.tar.gz"
-    sha256 "379943b8079821f6c151908b9b4b8abe834657f2b297c564daa3673f418b2d68"
+    url "https://github.com/utkuozdemir/pv-migrate/releases/download/v0.5.4/pv-migrate_0.5.4_darwin_x86_64.tar.gz"
+    sha256 "57b390d43afe86a12663e753e83ea31fe67d86b7876b889a8c43d8fe703c0941"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/utkuozdemir/pv-migrate/releases/download/v0.5.4/pv-migrate_v0.5.4_linux_x86_64.tar.gz"
-    sha256 "98c817140568e2a3137d3f745041ae79db41da3a149100ff95eed7d83e1d1c50"
+    url "https://github.com/utkuozdemir/pv-migrate/releases/download/v0.5.4/pv-migrate_0.5.4_linux_x86_64.tar.gz"
+    sha256 "33b749c3514cfd7e93279856d20e19f7101d8bcd16a5c44083bf48a80b3f75ef"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/utkuozdemir/pv-migrate/releases/download/v0.5.4/pv-migrate_v0.5.4_linux_arm64.tar.gz"
-    sha256 "850a46e5a6714ca5642315475bb1e6fb884c4d896f87d1fb69bd07388df0561c"
+    url "https://github.com/utkuozdemir/pv-migrate/releases/download/v0.5.4/pv-migrate_0.5.4_linux_arm64.tar.gz"
+    sha256 "bd6d19856e4677f93536f9d93df4984dbc3f6a59cf34ab406b57a824775afbc0"
   end
 
   def install
